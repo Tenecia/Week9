a4d59d1103a41fbe679786f4aead66530eb2c391
Tenecia Guinea <aja.guinea@gmail.com>
2015-08-23 13:26:59 -0400

    Exceptions

---
+++ CarsInClassException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarsDLL;
+
+namespace CarsInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars myCar = new Cars(); // 
+            myCar.Drive();
+            Cars myCar2 = new Cars();
+            myCar2.Stop(45);
+            myCar2.Stop(0);
+            Console.WriteLine();
+            //Console.ReadLine();
+            Console.WriteLine("***Simple Exception Example***");
+            Console.WriteLine("=>Creating a Car and then stepping on it!");
+            Cars myCar3 = new Cars();
+
+            // for (int i = 0; i < 10; i++)
+           // myCar3.Accellerate(10);
+            //Console.ReadLine();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar3.Accellerate(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n***Error***");
+                Console.WriteLine("Method:{0}", e.TargetSite);
+                Console.WriteLine("Message:{0}", e.Message);
+                Console.WriteLine("Source:{0}", e.Source);
+                Console.WriteLine("Help Link:{0}", e.HelpLink);
+            }
+            Console.WriteLine("\n****Out of exception logic****");
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
---
+++ DLL2CarsException.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsDLL
+{
+    public class Cars
+    {
+        private int speed;
+        private bool lights;
+        private int doors;
+        private string color;
+        public const int MaxSpeed = 100;
+        private string carName;
+        private bool carIsDead;
+
+        public string CarName // petname
+        {
+            get { return this.carName; }
+            set { this.carName = value; }
+        }
+
+        public int Speed // 1 st Property 
+        {
+            get { return this.speed; }
+            set { this.speed = value; }
+        }
+        public bool Lights // 1 st Property 
+        {
+            get { return this.lights; }
+            set { this.lights = value; }
+        }
+        public int Doors // 1 st Property 
+        {
+            get { return this.doors; }
+            set { this.doors = value; }
+        }
+        public string Color // 1 st Property 
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public Cars()  // 1st Constructor 
+        {
+            this.Speed = 45;
+            this.Lights = false;
+            this.Doors = 4;
+            this.Color = "Midnight Blue";
+            this.CarName = "Hot Wheels!";
+        }
+
+        public Cars(int Speed, bool Lights, int Doors, string Color, string Name) // Second Constructor 
+        {
+            this.Speed = Speed;
+            this.Lights = Lights;
+            this.Doors = Doors;
+            this.Color = Color;
+            this.CarName = Name;
+        }
+        public bool Stop(int Speed) // Method 1, static 
+        {
+            if (Speed >= 1)
+            {
+                this.Lights = true;
+            }
+
+            else
+            {
+                this.Lights = false;
+            }
+            Console.WriteLine(" Your car is stopped, are your lights on?  " + this.Lights);
+            return true;
+        }
+
+        public void Drive() // Method 2
+        {
+            Console.WriteLine("Our car is   " + this.Color + "   and drives   " + this.Speed + "   mph");
+        }
+
+        public void Accellerate(int delta) // Method 3
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", CarName);
+            else
+            {
+                Speed += delta;
+                if (Speed >= MaxSpeed)
+                {
+                    
+                    Speed = 0;
+                    carIsDead = true;
+                    Exception ex =
+                        new Exception(string.Format("{0} has overheated!", CarName));
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    throw ex;
+                }
+                else
+                    Console.WriteLine("=> Speed = {0}", Speed);
+                
+            }
+
+        }
+    }
+}
+
