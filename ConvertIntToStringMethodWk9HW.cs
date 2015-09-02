1ceddbca9af3958ae320131f2480f0529153138a
Tenecia <aja.guinea@gmail.com>
2015-09-02 19:14:42 -0400

    Week9HW

---
+++ ConvertIntToStringMethodWk9HW.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWcovertIntToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Write a function that converts an int to a string. Call it from Main. Upload it to GitHub.
+            int otherint = int.Parse(Console.ReadLine());
+            string myConvert = IntToString(otherint);
+            Console.WriteLine(myConvert);
+            Console.ReadLine();
+
+        }
+
+        public static String IntToString(int a)
+        {
+           int myint = a;
+           string mystring = Convert.ToString(myint);
+            return mystring;            
+
+        }
+
+    } 
+
+}
